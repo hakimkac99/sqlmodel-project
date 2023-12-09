46b4c056a1c0bf457cf9a2adc5c5b1c01a2712fb
Mohammed Abdelhakim KACEMI <76773173+hakimkac99@users.noreply.github.com>
2023-12-09 01:34:25 +0100

    :sparkles: add hero abilities endpoints (#5)
    * :sparkles: add hero abilities endpoints
    
    * :hammer: add hero abilities PUT endpoint

--- app/models/link_models.py
+++ app/models/link_models.py
@@ -1,10 +1,14 @@
 from sqlmodel import Field, SQLModel
 
 
-class HeroAbilityLink(
-    SQLModel,
-    table=True,
-):
+class HeroAbilityLink(SQLModel, table=True):
     __tablename__ = "hero_ability_link"
     hero_id: int | None = Field(default=None, foreign_key="hero.id", primary_key=True)
     ability_id: int | None = Field(default=None, foreign_key="ability.id", primary_key=True)
+    level: int
+
+
+class HeroAbilityLinkData(SQLModel):
+    hero_id: int
+    ability_id: int
+    level: int | None = Field(default=1, ge=1, le=9)
